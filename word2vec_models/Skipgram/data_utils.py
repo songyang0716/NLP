1f50881701b649914a22d0a8830f1e7f44296bc5
songyang0716 <yangsong0716@yahoo.com>
2020-05-10 13:03:20 -0700

    add working on skipgram

--- word2vec_models/Skipgram/data_utils.py
+++ word2vec_models/Skipgram/data_utils.py
@@ -8,3 +8,15 @@ from io import open
 import numpy as np
 
 
+
+def read_own_data(filename):
+    """
+    read your own data.
+    :param filename:
+    :return:
+    """
+    print('reading data...')
+    with open(filename, 'r', encoding='utf-8') as f:
+    	data = f.read()
+    print("corpus size", len(data))
+    return data 
--- word2vec_models/Skipgram/word2vec.py
+++ word2vec_models/Skipgram/word2vec.py
@@ -1,11 +1,20 @@
 ################################################################################################################
 #### Reference: https://github.com/blackredscarf/pytorch-SkipGram/blob/master/word2vec.py
 ################################################################################################################
+from data_utils import read_own_data
+
 class Word2Vec:
 	def __init__(self, path, vocab_size, embedding_size=100, learning_rate=1.0):
 		self.corpus = read_own_data(path)
-		self.data, self.word_count, self.word2index, self.index2word = build_dataset(self.corpus, vocab_size)
-		self.model = SkipGramNeg(vocabulary_size, embedding_size).cuda()
-		self.model_optim = SGD(self.model.parameters(), lr=learning_rate)
+		#self.data, self.word_count, self.word2index, self.index2word = build_dataset(self.corpus, vocab_size)
+		#self.model = SkipGramNeg(vocabulary_size, embedding_size).cuda()
+		#self.model_optim = SGD(self.model.parameters(), lr=learning_rate)
 
-	def train(self)
+	def train(self):
+		print("abc")
+
+
+review_path = '/Users/yangsong/Desktop/Projects/gitrepo_songyang0716/NLP/word2vec_models/yelp_review_10000.txt'
+w2v = Word2Vec(review_path, vocab_size=300 )
+w2v.train()
+print(w2v.corpus)
