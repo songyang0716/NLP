42f72989f3cb59f852dc07cc40a3e61a50cb493c
songyang0716 <yangsong0716@yahoo.com>
2020-05-10 19:36:02 -0700

    keep working on skipgram

--- word2vec_models/Skipgram/model.py
+++ word2vec_models/Skipgram/model.py
@@ -10,6 +10,16 @@ import torch.nn.functional as F
 class SkipGramNeg(nn.Module):
     def __init__(self, vocab_size, emb_dim):
         super(SkipGramNeg, self).__init__()
+        self.input_emb = nn.Embedding(vocab_size, emb_dim)
+        self.output_emb = nn.Embedding(vocab_size, emb_dim)
+        self.log_sigmoid = nn.LogSigmoid()
+
+          # Xavier init
+        self.input_emb.weight.data = nn.init.xavier_uniform_(self.input_emb.weight.data)
+        self.output_emb.weight.data = nn.init.xavier_uniform_(self.output_emb.weight.data)
+
+
+
 
 
     def forward(self, target_input, context, neg):
@@ -23,6 +33,17 @@ class SkipGramNeg(nn.Module):
         v = self.input_emb(target_input)
         u = self.output_emb(context)
 
+        positive_val = self.log_sigmoid(torch.sum(u * v, dim=1)).squeeze()
+
+		# u_hat: [batch_size, neg_size, emb_dim]
+		# for each positive sample, we will randomly pick k negative samples
+        u_hat = self.output_emb(neg)
+
+        loss = positive_val + neg_val
+        return -loss.mean()
+
 
     def predict(self, inputs):
         return self.input_emb(inputs)
+
+
