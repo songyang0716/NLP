d7231b525a22a3124d13c47898f6101536ff08c1
songyang0716 <yangsong0716@yahoo.com>
2020-05-16 21:20:42 -0700

    finish skipgram

--- word2vec_models/Skipgram/word2vec.py
+++ word2vec_models/Skipgram/word2vec.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 class Word2Vec:
-	def __init__(self, path, vocab_size=100, n_review=200, embedding_size=50, learning_rate=0.05):
+	def __init__(self, path, vocab_size=200, n_review=1000, embedding_size=50, learning_rate=0.1):
 		self.corpus = read_own_data(path)
 		self.corpus = self.corpus[:n_review]
 		self.data, self.word_count, self.word2index, self.index2word = build_dataset(self.corpus, vocab_size)
@@ -18,12 +18,6 @@ class Word2Vec:
 		self.model_optim = optim.SGD(self.model.parameters(), lr=learning_rate)
 
 		print("Number of token is: ", len(self.data))
-		# print(self.vocabs)
-		# print(len(self.vocabs))  51
-		# print(self.vocabs)       index [0,1,2,3....]
-		# print(len(self.word_count))  
-		# print(self.word_count)   default dict, word:count
-		# print(self.data)		   original text, use index	
 
 
 
@@ -37,7 +31,9 @@ class Word2Vec:
 		# self.outputdir = os.mkdir(output_dir)
 		pipeline = DataPipeline(self.data, self.vocabs ,self.word_count, self.word2index, self.index2word)
 		# used during the training analysis as validation samples
-		vali_example = random.sample(self.vocabs, vali_size)
+		# vali_example = random.sample(self.vocabs, vali_size)
+		# only check specific words
+		vali_example = np.array([self.word2index[word] for word in ['amazing','food','service','salad','sauce']])
 		# print(vali_example)
 		# [3, 20, 50]
 		n_batches = len(self.data) // batch_size
@@ -53,12 +49,6 @@ class Word2Vec:
 				batch_labels = torch.tensor(batch_labels, dtype=torch.long)
 				batch_neg = torch.tensor(batch_neg, dtype=torch.long)
 
-				# print("inputs")
-				# print(batch_inputs)
-				# print("labels")
-				# print(batch_labels)
-				# print("negative")
-				# print(batch_neg)
 
 				loss = self.model(batch_inputs, batch_labels, batch_neg)
 				self.model_optim.zero_grad()
@@ -68,43 +58,41 @@ class Word2Vec:
 				avg_loss += loss
 			avg_loss = avg_loss/n_batches
 
-			print('Total loss at current epoch : ', avg_loss)
+			if epoch % 100 == 0:
+				print("Current epoch:", epoch)
+				print("Total loss at current epoch:", avg_loss)
 
 			# if epoch % 100 == 0 and vali_size > 0:
-			if epoch == 0:
+			if epoch % 100 == 0 and epoch != 0:
 				print(vali_example)
 				self.most_similar(vali_example)
 
 
 
-	def most_similar(self, word_idx, top_k=5):
+	def most_similar(self, word_idx, top_k=3):
 		index = torch.tensor(word_idx, dtype=torch.long)
 		emb = self.model.predict(index).squeeze(0)
 		sim = torch.mm(emb, self.model.input_emb.weight.transpose(0, 1))
-		print(sim.shape)
-		print(sim)
-		sorted_index = torch.argsort(sim, dim=1, descending=True)
-		print(sorted_index.shape)
-		# print(emb)
-		# print(emb.squeeze(0).shape)
-		# print(self.model.input_emb.weight.shape)
-		# sim = torch.mm(emb, self.model.input_emb.weight.transpose(0, 1))
-		# nearest = (-sim[0]).sort()[1][1: top_k + 1]
-		# top_list = []
-		# for k in range(top_k):
-		# 	close_word = self.index2word[nearest[k].item()]
-		# 	top_list.append(close_word)
-		# return top_list
+		top_k_index = torch.argsort(sim, dim=1, descending=True)[:,:top_k]
+		for ix, wix in enumerate(word_idx):
+			print("For the word: ", self.index2word[wix], ". Its top k similar words are: ")
+			similar_words_idx = top_k_index[ix,:].detach().numpy()
+			for j in similar_words_idx:
+				print(self.index2word[j])
 
 
 review_path = '/Users/yangsong/Desktop/Projects/gitrepo_songyang0716/NLP/word2vec_models/yelp_review_10000.txt'
 w2v = Word2Vec(review_path)
-w2v.train(epochs=1)
-# print(w2v.corpus[0])
+w2v.train(epochs=801)
 
 
 
 
+# amazing
+# food
+# service
+# salad 
+# sauce
 
 
 
