6a02be6ee4895d599e56f63f4710e835c5f34b9c
songyang0716 <yangsong0716@yahoo.com>
2020-05-19 15:30:18 -0700

    working on glove

--- word2vec_models/glove/glove.py
+++ word2vec_models/glove/glove.py
@@ -12,11 +12,11 @@ from nltk.tokenize import word_tokenize
 l_rate = 0.001
 num_epochs = 10
 batch_size = 20
-alpha = 0.8
+alpha = 3/4
 context_size = 3
 embed_size = 100
 xmax = 2
-n_reviews = 10000
+n_reviews = 50
 
 
 
@@ -44,7 +44,7 @@ idx_to_word = {i:v for i, v in enumerate(vocab)}
 
 
 # cooccurence matrix
-cmat = np.zeros((vocab_size, vocab_size))
+comat = np.zeros((vocab_size, vocab_size))
 for i in range(word_size):
 	for j in range(1, context_size+1):
 		ind = word_to_idx[wordtokens[i]]
@@ -52,21 +52,36 @@ for i in range(word_size):
 			# left window - context_size words
 			lind = word_to_idx[wordtokens[i-j]] 
 			# provide some weight to the nearby words, the further, the smaller
-			cmat[ind, lind] += 1/j
+			comat[ind, lind] += 1/j
 		if i + j < word_size:
 			# right window - context_size words
 			rind = word_to_idx[wordtokens[i+j]]
-			cmat[ind, rind] += 1/j
-print(cmat.shape)
+			comat[ind, rind] += 1/j
+print(comat.shape)
 
 
-# some words 613 menu   465 steak   345 manager
-example_words = ['menu','steak','manager']
-for w in example_words:
-	print(w)
-	sww = cmat[word_to_idx[w],:]
-	# top 3 most similar words
-	sw = sww.argsort()[-3:][::-1]
-	print([idx_to_word[i] for i in sw])
+# non-zero occurence index
+coocs = np.transpose(np.nonzero(comat))
+
+
+
+
+
+
+# some helper functions
+def show_similar_words(words, comat, top_n=3):
+	for w in words:
+		print(w)
+		sww = comat[word_to_idx[w],:]
+		sw = sww.argsort()[-top_n:][::-1]
+		print([idx_to_word[i] for i in sw])
+
+def wf(x):
+	if x < xmax:
+		return (x/xmax)**alpha
+	else:
+		return 1
+
+
 
 
