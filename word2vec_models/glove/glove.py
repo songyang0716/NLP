fb4b7af2599506669770b45e49513a0452e2af52
songyang0716 <yangsong0716@yahoo.com>
2020-05-19 21:23:02 -0700

    finish glove model

--- word2vec_models/glove/glove.py
+++ word2vec_models/glove/glove.py
@@ -5,18 +5,19 @@ import random
 import numpy as np
 import spacy
 from nltk.tokenize import word_tokenize
-
+from model import GloVe
 
 
 # Parameters
 l_rate = 0.001
-num_epochs = 10
+num_epochs = 100
 batch_size = 20
-alpha = 3/4
-context_size = 3
-embed_size = 100
+alpha = 0.75
+context_size = 5
+window_size = 10
+embed_size = 2
 xmax = 100
-n_reviews = 50
+n_reviews = 5000
 
 
 
@@ -39,19 +40,73 @@ idx_to_word = {i:v for i, v in enumerate(vocab)}
 
 
 
-# Helper functions
-def show_similar_words(words, comat, top_n=3):
-	for w in words:
-		print(w)
-		sww = comat[word_to_idx[w],:]
-		sw = sww.argsort()[-top_n:][::-1]
-		print([idx_to_word[i] for i in sw])
 
-def wf(x):
-	if x < xmax:
-		return (x/xmax)**alpha
-	else:
-		return 1
+# cooccurence matrix
+comat = np.zeros((vocab_size, vocab_size))
+for i in range(word_size):
+	for j in range(1, context_size+1):
+		ind = word_to_idx[wordtokens[i]]
+		if i - j > 0:
+			# left window - context_size words
+			lind = word_to_idx[wordtokens[i-j]] 
+			# provide some weight to the nearby words, the further, the smaller
+			comat[ind, lind] += 1/j
+		if i + j < word_size:
+			# right window - context_size words
+			rind = word_to_idx[wordtokens[i+j]]
+			comat[ind, rind] += 1/j
+
+
+comat = torch.from_numpy(comat)
+print(comat.shape)
+
+# non-zero occurence index
+# coocs = np.transpose(np.nonzero(comat))
+model = GloVe(comat, embed_size)
+model_optim = optim.Adam(model.parameters(), lr=l_rate)
+
+
+
+
+
+def get_batch(batch_idx):
+	# word_to_idx[wordtokens[i]]
+	c_word_idx = []
+	context_word_idx = []
+	for i in range(batch_idx*batch_size, (batch_idx+1)*batch_size, 1):
+		for j in range(1, window_size+1):
+			ind = word_to_idx[wordtokens[i]]
+		if i - j > 0:
+			c_word_idx.append(ind)
+			context_word_idx.append(word_to_idx[wordtokens[i-j]])
+		if i + j < word_size:
+			c_word_idx.append(ind)
+			context_word_idx.append(word_to_idx[wordtokens[i+j]])
+
+
+	return c_word_idx, context_word_idx
+
+	# print(c_word_index)
+	# return "",""
+
+# train the model
+for epoch in range(num_epochs):
+	n_batches = int(word_size/batch_size)
+	avg_loss = 0
+	for j in range(n_batches):
+		input_index, output_index = get_batch(j)
+		input_index = torch.FloatTensor(input_index).to(torch.int64)
+		output_index = torch.FloatTensor(output_index).to(torch.int64)
+		model_optim.zero_grad()
+		loss = model(input_index, output_index)
+		loss.backward()
+		model_optim.step()
+		avg_loss += loss
+
+	avg_loss = avg_loss/n_batches
+	print(avg_loss)
+
+
 
 # def gen_batch(coocs):
 # 	# extract f
@@ -68,32 +123,16 @@ def wf(x):
 # 	return l_vecs, r_vecs, covals, l_v_bias, r_v_bias
 
 
-
-# cooccurence matrix
-comat = np.zeros((vocab_size, vocab_size))
-for i in range(word_size):
-	for j in range(1, context_size+1):
-		ind = word_to_idx[wordtokens[i]]
-		if i - j > 0:
-			# left window - context_size words
-			lind = word_to_idx[wordtokens[i-j]] 
-			# provide some weight to the nearby words, the further, the smaller
-			comat[ind, lind] += 1/j
-		if i + j < word_size:
-			# right window - context_size words
-			rind = word_to_idx[wordtokens[i+j]]
-			comat[ind, rind] += 1/j
-print(comat.shape)
-
-
-# non-zero occurence index
-coocs = np.transpose(np.nonzero(comat))
-
-
-
 # print(len(coocs))
 # gen_batch()
 
+# Helper functions
+def show_similar_words(words, comat, top_n=3):
+	for w in words:
+		print(w)
+		sww = comat[word_to_idx[w],:]
+		sw = sww.argsort()[-top_n:][::-1]
+		print([idx_to_word[i] for i in sw])
 
 
 
--- word2vec_models/glove/model.py
+++ word2vec_models/glove/model.py
@@ -8,22 +8,22 @@ import torch.nn.functional as F
 
 
 class GloVe(nn.Module):
+	def __init__(self, co_oc, emb_dim, x_max=100, alpha=0.75):
 		"""
 		:param co_oc: Co-occurrence ndarray with shape of [num_classes, num_classes]
 		:param embed_size: embedding size
 		:param x_max: An int representing cutoff of the weighting function
 		:param alpha: Ant float parameter of the weighting function
 		"""
-
-	def __init__(self, co_oc, emb_dim, x_max=100, alpha=0.75):
 		super(GloVe, self).__init__()
 
 		self.emb_dim = emb_dim
 		self.x_max = x_max
 		self.alpha = alpha
+		self.co_oc = co_oc.float()
 
 		# to prevent the log(0) issue
-		self.co_oc = self.co_oc + 1
+		self.co_oc = self.co_oc + 1.0
 		self.num_classes = self.co_oc.shape[0]
 
 		self.input_emb = nn.Embedding(self.num_classes, self.emb_dim)
@@ -47,23 +47,25 @@ class GloVe(nn.Module):
 		:return: loss estimation for Global Vectors word representations
 				 defined in nlp.stanford.edu/pubs/glove.pdf
 		"""
+
 		batch_size = len(input_idx)
-		input_batch_embed = self.input_emb[input_idx]
-		input_batch_bias = self.input_bias[input_idx]
+		# print(input_idx)
+		input_batch_embed = self.input_emb(input_idx)
+		input_batch_bias = self.input_bias(input_idx)
 
-		output_batch_embed = self.output_emb[output_idx]
-		output_batch_bias = self.output_bias[output_idx]
+		output_batch_embed = self.output_emb(output_idx)
+		output_batch_bias = self.output_bias(output_idx)
 
-		output_matrix = torch.sum(torch.mul(input_batch_embed, output_batch_embed), dim=1) + input_batch_bias + output_batch_bias - tensor.log(self.co_oc[input_idx, output_idx])
+		output_matrix = torch.sum(torch.mul(input_batch_embed, output_batch_embed), dim=1) + input_batch_bias + output_batch_bias - torch.log(self.co_oc[input_idx, output_idx])
 		weight_matrix = self._weight(input_idx, output_idx)
 		loss = output_matrix * output_matrix * weight_matrix
 		return loss.mean()
 
 	def _weight(self, input_idx, output_idx):
-		weights = torch.ones(size=len(input_idx))
-		weights[self.co_oc[input_idx, output_idx] <= self.x_max] = (self.co_oc[input_idx, output_idx] / self.x_max) ** self.alpha
+
+		weights = torch.ones(len(input_idx))
+		weights[self.co_oc[input_idx, output_idx] <= self.x_max] = (self.co_oc[input_idx, output_idx][self.co_oc[input_idx, output_idx] <= self.x_max] / self.x_max) ** self.alpha
 		return weights
 
-
-
-
+	def embeddings(self):
+		return self.input_emb.weight.data.cpu().numpy()
