ccea7e4d72efe60d711da5cb681cf36b7cc3002e
songyang0716 <yangsong0716@yahoo.com>
2020-05-23 21:24:23 -0700

    working on lstm

--- text_generation/LSTM.py
+++ text_generation/LSTM.py
@@ -12,7 +12,7 @@ from nltk.tokenize import word_tokenize
 # Parameters
 train_file='yelp_review_10000.txt'
 seq_size=32
-batch_size=50
+batch_size=16
 embedding_size=64
 lstm_size=64
 gradients_norm=5
@@ -25,6 +25,7 @@ def read_file(train_file, batch_size, seq_size):
 	with open(train_file, "r", encoding='utf-8') as f:
 		reviews = f.read()
 	words = reviews.split()
+	words = [word.lower() for word in words]
 	words_freq = Counter(words)
 	words_freq = sorted(words_freq.items(), key=lambda x: x[1], reverse=True)
 
@@ -34,18 +35,32 @@ def read_file(train_file, batch_size, seq_size):
 	word_to_idx = {v:i for i, v in enumerate(vocab)}
 	idx_to_word = {i:v for i, v in enumerate(vocab)}
 
-	print('Vocabulary size', vocab_size)
+	word_index = [word_to_idx[w] for w in words]
+	num_batches = int(len(word_index) / (seq_size * batch_size))
 
-	return
-	# print(len(reviews))
-	# return 
+	in_text = word_index[:num_batches * batch_size * seq_size]
+	out_text = np.zeros_like(in_text)
+
+	out_text[:-1] = in_text[1:]
+	out_text[-1] = in_text[0]
+
+	in_text = np.reshape(in_text,(num_batches,-1))
+	out_text = np.reshape(out_text,(num_batches,-1))
+
+	print(in_text.shape)
+	print(out_text.shape)
+
+	return idx_to_word, word_to_idx, vocab_size, in_text, out_text
+
 
 
 def main():
 	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 	# nlp = spacy.load("en_core_web_sm")
+	idx_to_word, word_to_idx, vocab_size, in_text, out_text = read_file(train_file, batch_size, seq_size)
 
-	read_file(train_file, batch_size, seq_size)
+
+
 
 if __name__ == '__main__':
     main()
