1bdf91cf8430998a0670fc30fc0ab547d8eb138d
songyang0716 <yangsong0716@yahoo.com>
2020-05-23 21:27:18 -0700

    working on lstm

---
+++ text_generation/model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as T
+import torch.nn.functional as F
+
+class LSTM(nn.Module):
+	def __init__(self):
+
+	def forward(self, input_idx, output_idx):
+
+	
