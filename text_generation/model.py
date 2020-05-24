934c4ebcd786c99047a4090a3d8c8fafa648eb54
songyang0716 <yangsong0716@yahoo.com>
2020-05-23 21:43:51 -0700

    working on lstm model

--- text_generation/model.py
+++ text_generation/model.py
@@ -4,8 +4,67 @@ import torchvision.transforms as T
 import torch.nn.functional as F
 
 class LSTM(nn.Module):
-	def __init__(self):
+	def __init__(self, n_vocab, seq_size, emb_size, hidden_size):
+		super(LSTM, self).__init__()
+		self.seq_size = seq_size
+		self.lstm_size = hidden_size
+		self.embedding = nn.Embedding(n_vocab, emb_size)
 
-	def forward(self, input_idx, output_idx):
+		# if batch_first=True, input as (batch, seq, feature)
+		self.lstm = nn.LSTM(input_size=emb_size,
+							hidden_size=hidden_size,
+							num_layers=1,
+							batch_first=True)
 
-	
+		# from hidden layer to output layer
+		# hidden_size to n_vocab
+		self.dense = nn.Linear(hidden_size, n_vocab)
+
+
+		# Xavier init
+		self.embedding.weight.data = nn.init.xavier_uniform_(self.embedding.weight.data)
+		self.lstm.weight.data = nn.init.xavier_uniform_(self.lstm.weight.data)
+		self.dense.weight.data = nn.init.xavier_uniform_(self.dense.weight.data)
+
+
+	def forward(self, x, prev_state):
+		embed = self.embedding(x)
+		output, state = self.lstm(embed, prev_state)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
