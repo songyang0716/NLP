a3bafb6029505a3db68a06eb2448ae2fc14aff50
songyang0716 <yangsong0716@yahoo.com>
2020-05-26 14:42:28 -0700

    start sentiment classification using bilstm

---
+++ sentiment_classification/BiLSTM/bilstm.py
@@ -0,0 +1,13 @@
+import sys
+import torch
+import torch.optim as optim
+import random
+import numpy as np
+import spacy
+from collections import Counter
+from nltk.tokenize import word_tokenize
+# from model import LSTM 
+
+
+
+
