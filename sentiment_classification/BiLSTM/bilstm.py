53ea9b058524b2c3fb2b6cdebd1e6087af75146a
songyang0716 <yangsong0716@yahoo.com>
2020-06-04 21:58:08 -0700

    keep working on BiLSTM

--- sentiment_classification/BiLSTM/bilstm.py
+++ sentiment_classification/BiLSTM/bilstm.py
@@ -11,3 +11,31 @@ from nltk.tokenize import word_tokenize
 
 
 
+def train(model, training_data, args, optimizer, criterion):
+    model.train()
+
+    batch_size = args.batch_size
+
+    sentences, sentences_seqlen, sentences_mask, labels = training_data
+
+    # print batch_size, len(sentences), len(labels)
+
+    assert batch_size == len(sentences) == len(labels)
+
+    ''' Prepare data and prediction'''
+    sentences_, sentences_seqlen_, sentences_mask_ = \
+        var_batch(args, batch_size, sentences, sentences_seqlen, sentences_mask)
+    labels_ = Variable(torch.LongTensor(labels))
+    if args.cuda:
+        labels_ = labels_.cuda()
+
+    assert len(sentences) == len(labels)
+
+    model.zero_grad()
+    probs = model(sentences_, sentences_seqlen_, sentences_mask_)
+    loss = criterion(probs.view(len(labels_), -1), labels_)
+
+    loss.backward()
+    optimizer.step()
+
+    
