968b168a544bbb3506efeafd9365374b1d185469
songyang0716 <yangsong0716@yahoo.com>
2020-07-04 22:29:59 -0700

    keep working on ESIM training model

--- natural_language_inference/ESIM/model.py
+++ natural_language_inference/ESIM/model.py
@@ -1,1 +1,20 @@
 # Reference code: https://github.com/coetaur0/ESIM
+
+import torch
+import torch.nn as nn
+# import torchvision.transforms as T
+import torch.nn.functional as F
+
+class ESIM(nn.Module):
+	"""
+		Implementation of CNN for classification
+	"""
+	def __init__(self, 
+				 embeddings_size,
+				 embeddings_dim,
+				 hidden_size,
+				 embeddings,
+				 dropout,
+				 num_classes,
+				 device):
+		super(ESIM, self).__init__()
--- natural_language_inference/ESIM/train.py
+++ natural_language_inference/ESIM/train.py
@@ -10,6 +10,63 @@ from preprocessing import NLIDataset
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
+from model import ESIM
+from tqdm import tqdm
+
+
+def train(model,
+		  dataloader,
+		  optimizer,
+		  criterion,
+		  epoch_number,
+		  max_gradient_norm):
+	"""
+	Train a model for one epoch on some input data with a given optimizer and
+	criterion.
+	Args:
+		model: A torch module that must be trained on some input data.
+		dataloader: A DataLoader object to iterate over the training data.
+		optimizer: A torch optimizer to use for training on the input model.
+		criterion: A loss criterion to use for training.
+		epoch_number: The number of the epoch for which training is performed.
+		max_gradient_norm: Max. norm for gradient norm clipping.
+	Returns:
+		epoch_time: The total time necessary to train the epoch.
+		epoch_loss: The training loss computed for the epoch.
+		epoch_accuracy: The accuracy computed for the epoch.
+	"""
+	# Switch the model to train mode.
+	model.train()
+	device = model.device
+
+	epoch_start = time.time()
+	batch_time_avg = 0.0
+	running_loss = 0.0
+	correct_preds = 0
+	tqdm_batch_iterator = tqdm(dataloader)
+
+	for batch_index, batch in enumerate(tqdm_batch_iterator):
+		batch_start = time.time()
+
+		# Move input and output data to the GPU if it is used.
+		premises = batch["premise"].to(device)
+		premises_lengths = batch["premise_length"].to(device)
+		hypotheses = batch["hypothesis"].to(device)
+		hypotheses_lengths = batch["hypothesis_length"].to(device)
+		labels = batch["label"].to(device)
+
+		optimizer.zero_grad()
+
+		logits, probs = model(premises,
+							  premises_lengths,
+							  hypotheses,
+							  hypotheses_lengths)
+		loss = criterion(logits, labels)
+		loss.backward()
+
+
+
+	
 def main(train_file,
 		 valid_file,
 		 embeddings_file,
@@ -17,7 +74,7 @@ def main(train_file,
 		 hidden_size=300,
 		 dropout=0.5,
 		 num_classes=3,
-		 epochs=64,
+		 epochs=50,
 		 batch_size=32,
 		 lr=0.0004,
 		 patience=5,
@@ -65,9 +122,62 @@ def main(train_file,
 
 	valid_loader = DataLoader(valid_data, shuffle=False, batch_size=batch_size)
 
-	print(train_loader)
+	# print(train_loader)
+	# -------------------- Model definition ------------------- #
+	print("\t* Building model...")
+	with open(embeddings_file, "rb") as pkl:
+		embeddings = torch.tensor(pickle.load(pkl), dtype=torch.float)\
+					 .to(device)
+					 
+	print(embeddings.size())
 	
+	esim_model = ESIM(embeddings.shape[0],
+					  embeddings.shape[1],
+					  hidden_size,
+					  embeddings,
+					  dropout,
+					  num_classes,
+					  device).to(device)
 
+	# -------------------- Preparation for training  ------------------- #
+	criterion = nn.CrossEntropyLoss()
+	optimizer = optim.Adam(esim_model.parameters(), lr=l_rate, weight_decay=1e-5)
+	scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+														   mode="max",
+														   factor=0.5,
+														   patience=0)
+
+	best_score = 0.0
+	start_epoch = 1
+
+	# Data for loss curves plot.
+	epochs_count = []
+	train_losses = []
+	valid_losses = []
+
+	# -------------------- Training epochs ------------------- #
+	print("\n",
+		  20 * "=",
+		  "Training ESIM model on device: {}".format(device),
+		  20 * "=")
+
+
+	patience_counter = 0
+	for epoch in range(start_epoch, epochs+1):
+		epochs_count.append(epoch)
+
+		print("* Training epoch {}:".format(epoch))
+		epoch_time, epoch_loss, epoch_accuracy = train(esim_model,
+													   train_loader,
+													   optimizer,
+													   criterion,
+													   epoch,
+													   max_grad_norm)
+
+		# nn.utils.clip_grad_norm_(model.parameters(), max_gradient_norm)
+		optimizer.step()
+		
+		
 
 if __name__ == '__main__':
 	main(train_file='./../data/train_data.pkl',
