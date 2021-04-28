4af1d7f6da9db746a12c62d4dfe83cafe4e10d43
yangsong0716 <yangsong0716@yahoo.com>
2021-04-27 22:19:01 -0700

    finish intra attention model and start hyperparameter tunning

--- natural_language_inference/BiLSTM_encoders_with_intra-attention/model.py
+++ natural_language_inference/BiLSTM_encoders_with_intra-attention/model.py
@@ -25,24 +25,19 @@ class encoder(nn.Module):
                                     dropout=p,
                                     bidirectional=True)
 
-    def forward(self, prem_batch, sequence_length):
+    def forward(self, sentence_batch, sequence_length):
         """
             Feed word embedding batch into LSTM
         """
-        prem_embedding_layer = self.dropout(self.emb(prem_batch))
+        sentence_embedding_layer = self.dropout(self.emb(sentence_batch))
         packed_embeddings = \
-            nn.utils.rnn.pack_padded_sequence(prem_embedding_layer,
+            nn.utils.rnn.pack_padded_sequence(sentence_embedding_layer,
                                               sequence_length.cpu(),
                                               batch_first=True,
                                               enforce_sorted=False)
-        prem_out, (prem_hn, prem_cn) = self.bilstm_layer(packed_embeddings)
-        print("prem_out shape before packed", prem_out.size())
-
-        prem_out, _ = nn.utils.rnn.pad_packed_sequence(prem_out,
-                                                       batch_first=True)
-        print("prem_out shape", prem_out.size())
-
-        return prem_out, prem_hn, prem_cn
+        out, (hn, cn) = self.bilstm_layer(packed_embeddings)
+        out, _ = nn.utils.rnn.pad_packed_sequence(out, batch_first=True)
+        return out, hn, cn
 
 
 class inner_attention(nn.Module):
@@ -58,12 +53,26 @@ class inner_attention(nn.Module):
         self.WY = nn.Linear(2 * hidden_size, 2 * hidden_size)
         self.WH = nn.Linear(2 * hidden_size, 2 * hidden_size)
         self.W = nn.Linear(2 * hidden_size, 1)
+        self.fc = nn.Linear(8 * hidden_size, 3)
         self.tanh = nn.Tanh()
         self.softmax_layer = nn.Softmax(dim=1)
 
+        self.WY.weight.data = nn.init.xavier_uniform_(self.WY.weight.data)
+        self.WH.weight.data = nn.init.xavier_uniform_(self.WH.weight.data)
+        self.W.weight.data = nn.init.xavier_uniform_(self.W.weight.data)
+        self.fc.weight.data = nn.init.xavier_uniform_(self.fc.weight.data)
+
     def forward(self, prem_batch, hyp_batch, prem_length, hyp_length):
         """
-            Perform inner attention
+            Perform inner attention on premise and hyp batch
+
+            Arguments:
+                prem_batch:  premise sentence indexes
+                hyp_batch:   hypothesis sentence indexes
+                prem_length: premise sentence length
+                hyp_length:  hypothesis sentence length
+            Returns:
+                inner attentioned sentences
         """
         prem_bilstm_output, _, _ = self.encoder(prem_batch, prem_length)
         hyp_bilstm_output, _, _ = self.encoder(hyp_batch, hyp_length)
@@ -77,24 +86,32 @@ class inner_attention(nn.Module):
             self.WY(prem_bilstm_output) + \
             self.WH(torch.matmul(prem_mean_vec,
                                  torch.ones([1,
-                                             prem_bilstm_output.shape[1]])).permute(0,2,1))
+                                             prem_bilstm_output.shape[1]], device='cuda')).permute(0,2,1))
         # Size of M is like  batch * sequence * embedding
         M_premise = self.tanh(M_premise)
         # Size of weights premise is like batch * sequence
         weights_premise = self.softmax_layer(self.W(M_premise).squeeze(2))
         weighted_premise = torch.bmm(M_premise.permute(0, 2, 1), weights_premise.unsqueeze(2))
+        weighted_premise = weighted_premise.squeeze(2)
 
         M_hyp = \
             self.WY(hyp_bilstm_output) + \
             self.WH(torch.matmul(hyp_mean_vec,
                                  torch.ones([1,
-                                             hyp_bilstm_output.shape[1]])).permute(0,2,1))
+                                             hyp_bilstm_output.shape[1]], device='cuda')).permute(0,2,1))
         # Size of M is like  batch * sequence * embedding
         M_hyp = self.tanh(M_hyp)
         # Size of weights premise is like batch * sequence
         weights_hyp = self.softmax_layer(self.W(M_hyp).squeeze(2))
         weighted_hyp = torch.bmm(M_hyp.permute(0, 2, 1), weights_hyp.unsqueeze(2))
+        weighted_hyp = weighted_hyp.squeeze(2)
 
-        return weighted_hyp.squeeze(2) + weighted_premise.squeeze(2)
+        sentence_difference = weighted_premise - weighted_hyp
+        sentence_multiplication = weighted_premise * weighted_hyp
 
-
+        second_last_out = torch.cat((weighted_premise,
+                                     sentence_multiplication,
+                                     sentence_difference,
+                                     weighted_hyp), dim=1)
+        out = self.fc(second_last_out)
+        return out
--- natural_language_inference/BiLSTM_encoders_with_intra-attention/preprocessing_training.py
+++ natural_language_inference/BiLSTM_encoders_with_intra-attention/preprocessing_training.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
 
 import torchtext.legacy as legacy
-from model import encoder
+from model import encoder, inner_attention
 
 
 # Set device
@@ -48,11 +48,10 @@ def check_accuracy(validation_iterator, model, step, writer):
 
     with torch.no_grad():
         for val_batch_idx, val_batch in enumerate(validation_iterator):
-            val_hyp = val_batch.hypothesis
-            val_prem = val_batch.premise
-
+            val_hyp, val_hyp_length = val_batch.hypothesis
+            val_prem, val_prem_length = val_batch.premise
             val_target = val_batch.label - 1
-            scores = model(val_hyp, val_prem)
+            scores = model(val_prem, val_hyp, val_prem_length, val_hyp_length)
             # return the indices of each prediction
             _, predictions = scores.max(1)
             num_correct += (predictions == val_target).sum()
@@ -69,11 +68,87 @@ embedding_size = 300
 load_model = False
 
 # Model hyperparameter
-num_layers = 1
+num_layer = 1
 p = 0.25
-learning_rates = 0.001
-batch_sizes = 128
+learning_rate = 0.001
+batch_size = 128
 hidden_sizes = 100
 num_epochs = 5
 
 
+for hidden_size in [hidden_sizes]:
+    step = 0
+    # define model structure
+    sentence_embedding = encoder(hidden_size,
+                                 pretrained_embeddings,
+                                 embedding_size,
+                                 num_layer,
+                                 p).to(device)
+
+    model = inner_attention(sentence_embedding).to(device)
+    model.train()
+    # define train iterator
+    train_iterator, validation_iterator, test_iterator = \
+        legacy.data.BucketIterator.splits((train_data,
+                                          validation_data,
+                                          test_data),
+                                          batch_size=batch_size,
+                                          device=device,
+                                          sort_within_batch=True)
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    writer = SummaryWriter('runs/SNLI/\
+        inner_attention_tensorboard/hidden_size_{}'.format(hidden_size))
+    if load_model:
+        load_checkpoint(torch.load("my_checkpoint.pth.tar"))
+
+    # Train Network
+    for epoch in range(num_epochs):
+        losses = []
+        accuracies = []
+        if (epoch + 1) % 5 == 0:
+            checkpoint = {'state_dict': model.state_dict(),
+                          'optimizer': optimizer.state_dict()}
+            save_checkpoint(checkpoint)
+        for batch_idx, batch in enumerate(train_iterator):
+            model.train()
+            # data
+            prem_sentences, prem_length = batch.premise
+            hyp_sentences, hyp_length = batch.hypothesis
+
+            # outcome data need to be between 0 - (n_class-1)
+            target = batch.label - 1
+            # forward
+            scores = model(prem_sentences, hyp_sentences,
+                           prem_length, hyp_length)
+            loss = criterion(scores, target)
+            losses.append(loss.item())
+
+            # backward
+            optimizer.zero_grad()
+            loss.backward()
+            nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
+            optimizer.step()
+
+            _, predictions = scores.max(1)
+            num_correct = (predictions == target).sum()
+            running_train_acc = float(num_correct) / \
+                float(hyp_sentences.shape[0])
+            accuracies.append(running_train_acc)
+
+            writer.add_scalar('Training Loss', loss, global_step=step)
+            writer.add_scalar('Training Accuracy',
+                              running_train_acc,
+                              global_step=step)
+            # step += 1
+            model.eval()
+            if batch_idx % 100 == 0:
+                writer.add_scalar('Training Loss', loss, global_step=step)
+                writer.add_scalar('Training Accuracy',
+                                  running_train_acc,
+                                  global_step=step)
+
+                # Check for the running accuracy of validation
+                check_accuracy(validation_iterator, model, step, writer)
+                step += 1
